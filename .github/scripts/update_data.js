994c9ebf2abadc4a16c46a0c40c3564232b8020e
Eli Cheramie <elicheramie89@gmail.com>
2025-10-31 05:38:00 -0400

    user-agent fetch fix and grid-maker update prep

--- .github/scripts/update_data.js
+++ .github/scripts/update_data.js
@@ -15,7 +15,7 @@ let downloadlist = [];
 const urls = {
     summons: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=summons&fields=id%2C_pageName%3DpageName%2Cname%2Cevo_max%3DmaxUncap%2Crarity%2Celement%2Cseries&formatversion=2",
     characters: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=characters&fields=_pageName%3DpageName%2Cid%2Cseries%2Cstyle_name%3DstyleName%2Cstyle_id%3DstyleId%2Cjpname%2Cname%2Crelease_date%3DreleaseDate%2Cgender%2Cobtain%2C5star_date%3DflbDate%2Cmax_evo%3DmaxUncap%2Cexpedition_type%3DexpeditionType%2Crarity%2Celement%2Ctype%2Ccustomtype%2Crace%2Cjoin_weapon%3DrecruitWeapon%2Cweapon%2Cart_ex%3Dartex%2Cart_bonus%3Dartbonus%2Cgender%2Ccharid&formatversion=2",
-    weapons: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=weapons&fields=id%2C_pageName%3DpageName%2Cevo_max%3DmaxUncap%2Crarity%2Cseries%2Celement%2Cs1_icon%2Cs2_icon%2Cs3_icon%2Ctype%2Cawakening%2Cawakening_type1%3DawakeningType1%2Cawakening_type2%3DawakeningType2%2Cs1_name%3Ds1Name%2Cs2_name%3Ds2Name%2Cs3_name%3Ds3Name&formatversion=2",
+    weapons: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=weapons&fields=id%2C_pageName%3DpageName%2Cevo_max%3DmaxUncap%2Crarity%2Cseries%2Celement%2Ccharacter_unlock%3Dcharacter%2Ctype%2Cawakening%2Cawakening_type1%3DawakeningType1%2Cawakening_type2%3DawakeningType2%2Cs1_icon%3Ds1Icon%2Cs2_icon%3Ds2Icon%2Cs3_icon%3Ds3Icon%2Cs1_name%3Ds1Name%2Cs2_name%3Ds2Name%2Cs3_name%3Ds3Name&formatversion=2",
     abilities: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=class_skill&fields=ix%2Cname%2C&formatversion=2"
 };
 
@@ -55,58 +55,33 @@ const jqQueries = {
             maxUncap: item.maxUncap
         }
     })).reduce((acc, curr) => Object.assign(acc, curr), {}),
-    characters: data => data.map(item => {
-        if (item.id != 3030182000) {
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/zoom/${item.id}_01.png`, `./assets/characters/full/${item.id}_01`);
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/zoom/${item.id}_02.png`, `./assets/characters/full/${item.id}_02`);
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/quest/${item.id}_01.jpg`, `./assets/characters/tall/${item.id}_01`);
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/quest/${item.id}_02.jpg`, `./assets/characters/tall/${item.id}_02`);
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/m/${item.id}_01.jpg`, `./assets/characters/icon/${item.id}_01`);
-            downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/m/${item.id}_02.jpg`, `./assets/characters/icon/${item.id}_02`);
-            if (item.maxUncap >= 5) {
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/zoom/${item.id}_03.png`, `./assets/characters/full/${item.id}_03`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/quest/${item.id}_03.jpg`, `./assets/characters/tall/${item.id}_03`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/m/${item.id}_03.jpg`, `./assets/characters/icon/${item.id}_03`);
-            }
-            if (item.maxUncap >= 6) {
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/zoom/${item.id}_04.png`, `./assets/characters/full/${item.id}_04`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/quest/${item.id}_04.jpg`, `./assets/characters/tall/${item.id}_04`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/m/${item.id}_04.jpg`, `./assets/characters/icon/${item.id}_04`);
-            }
-            if (item.artbonus != null) {
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/zoom/${item.id}_91.png`, `./assets/characters/full/${item.id}_91`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/quest/${item.id}_91.png`, `./assets/characters/full/${item.id}_91`);
-                downloadImage(`https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/npc/m/${item.id}_91.png`, `./assets/characters/full/${item.id}_91`);
-            }
+    characters: data => data.map(item => ({
+        [item.id]: {
+            pageName: item.pageName.replace(/&#039;/g, "'"),
+            name: item.name.replace(/&#039;/g, "'"),
+            maxUncap: item.maxUncap,
+            rarity: item.rarity,
+            element: item.element.trim(),
+            series: item.series,
+            styleName: item.styleName,
+            styleId: item.styleId,
+            jpname: item.jpname,
+            releaseDate: item.releaseDate,
+            gender: item.gender,
+            obtain: item.obtain,
+            flbDate: item.flbDate,
+            expeditionType: item.expeditionType,
+            type: item.type.trim(),
+            customType: item.customType,
+            race: item.race.map(r => r.trim()),
+            recruitWeapon: item.recruitWeapon ? item.recruitWeapon.replace(/&#039;/g, "'") : null,
+            weapon: item.weapon.map(w => w.trim()),
+            artbouns: item.artbouns ? true : false,
+            artex: item.artex ? true : false,
+            gender: item.gender,
+            charid: item.charid
         }
-        return {
-            [item.id]: {
-                pageName: item.pageName.replace(/&#039;/g, "'"),
-                name: item.name.replace(/&#039;/g, "'"),
-                maxUncap: item.maxUncap,
-                rarity: item.rarity,
-                element: item.element.trim(),
-                series: item.series,
-                styleName: item.styleName,
-                styleId: item.styleId,
-                jpname: item.jpname,
-                releaseDate: item.releaseDate,
-                gender: item.gender,
-                obtain: item.obtain,
-                flbDate: item.flbDate,
-                expeditionType: item.expeditionType,
-                type: item.type.trim(),
-                customType: item.customType,
-                race: item.race.map(r => r.trim()),
-                recruitWeapon: item.recruitWeapon ? item.recruitWeapon.replace(/&#039;/g, "'") : null,
-                weapon: item.weapon.map(w => w.trim()),
-                artbouns: item.artbouns ? true : false,
-                artex: item.artex ? true : false,
-                gender: item.gender,
-                charid: item.charid
-            }
-        }
-    }).reduce((acc, curr) => Object.assign(acc, curr), {}),
+    })).reduce((acc, curr) => Object.assign(acc, curr), {}),
     minCharacters: data => data.map(item => ({
         [item.id]: {
             pageName: item.pageName.replace(/&#039;/g, "'")
@@ -119,12 +94,19 @@ const jqQueries = {
             rarity: item.rarity,
             series: item.series,
             element: item.element,
-            s1Name: item.s1Name ? item.s1Name.replace(/&#039;/g, "'") : null,
-            s2Name: item.s2Name ? item.s2Name.replace(/&#039;/g, "'") : null,
-            s3Name: item.s3Name ? item.s3Name.replace(/&#039;/g, "'") : null,
-            "s1 icon": item["s1 icon"],
-            "s2 icon": item["s2 icon"],
-            "s3 icon": item["s3 icon"],
+            character: item.character,
+            skill1: {
+                name: item.s1Name ? item.s1Name.replace(/&#039;/g, "'") : null,
+                icon: item["s1Icon"],
+            },
+            skill2: {
+                name: item.s2Name ? item.s2Name.replace(/&#039;/g, "'") : null,
+                icon: item["s2Icon"],
+            },
+            skill3: {
+                name: item.s3Name ? item.s3Name.replace(/&#039;/g, "'") : null,
+                icon: item["s3Icon"],
+            },
             type: item.type,
             awakening: item.awakening,
             awakeningType1: item.awakeningType1,
@@ -145,7 +127,11 @@ const jqQueries = {
 
 async function fetchData(url) {
     let data = [];
-    await fetch(url, { USER_AGENT: USER_AGENT })
+    await fetch(url, {
+        headers: {
+            "User-Agent": USER_AGENT
+        }
+    })
         .then(response => response.json())
         .then(response => data = response)
         .catch(error => console.log(error));
