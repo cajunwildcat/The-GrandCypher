46164216f3fa1d0b3568010dfa225b7c0a1b62b0
Eli Cheramie <elicheramie89@gmail.com>
2026-02-11 17:05:05 -0500

    added classes json

--- .github/scripts/update_data.js
+++ .github/scripts/update_data.js
@@ -16,7 +16,8 @@ const urls = {
     summons: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=summons&fields=id%2C_pageName%3DpageName%2Cname%2Cevo_max%3DmaxUncap%2Crarity%2Celement%2Cseries%2Cjpname&formatversion=2",
     characters: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=characters&fields=_pageName%3DpageName%2Cid%2Cseries%2Cstyle_name%3DstyleName%2Cstyle_id%3DstyleId%2Cjpname%2Cname%2Crelease_date%3DreleaseDate%2Cgender%2Cobtain%2C5star_date%3DflbDate%2Cmax_evo%3DmaxUncap%2Cexpedition_type%3DexpeditionType%2Crarity%2Celement%2Ctype%2Ccustomtype%2Crace%2Cjoin_weapon%3DrecruitWeapon%2Cweapon%2Cart_ex%3Dartex%2Cart_bonus%3Dartbonus%2Cgender%2Ccharid&formatversion=2",
     weapons: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=weapons&fields=id%2C_pageName%3DpageName%2Cevo_max%3DmaxUncap%2Crarity%2Cseries%2Celement%2Ccharacter_unlock%3Dcharacter%2Ctype%2Cawakening%2Cawakening_type1%3DawakeningType1%2Cawakening_type2%3DawakeningType2%2Cs1_icon%3Ds1Icon%2Cs2_icon%3Ds2Icon%2Cs3_icon%3Ds3Icon%2Cs1_name%3Ds1Name%2Cs2_name%3Ds2Name%2Cs3_name%3Ds3Name%2Cjpname&formatversion=2",
-    abilities: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=class_skill&fields=ix%2Cname%2Cicon%2Cunique_key%3Did&formatversion=2"
+    abilities: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=class_skill&fields=ix%2Cname%2Cicon%2Cunique_key%3Did&formatversion=2",
+    classes: "https://gbf.wiki/index.php?title=Special:CargoExport&format=json&limit=5000&tables=classes&fields=name%2Cjpname%2Cid_num%3Did%2Cid%3Dimgid&formatversion=2",
 };
 
 const files = {
@@ -34,6 +35,9 @@ const files = {
     ],
     abilities: [
         { query: "abilities", file: "abilities.json" }
+    ],
+    classes: [
+        { query: "classes", file: "classes.json" }
     ]
 };
 
@@ -153,6 +157,13 @@ const jqQueries = {
             ix: item.ix,
             id: item.id,
             icon: item.icon
+        }
+    })).reduce((acc, curr) => Object.assign(acc, curr), {}),
+    classes: data => data.map(item => ({
+        [item.name.replace(/&#039;/g, "'")]: {
+            id: item.id,
+            imgid: item.imgid,
+            jpname: item.jpname
         }
     })).reduce((acc, curr) => Object.assign(acc, curr), {})
 };
